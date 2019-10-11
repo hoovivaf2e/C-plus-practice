1ba2cef9315bc6a94e7569bca7aa710363f5b5bc
Eileen <hooviva77@gmail.com>
2019-10-11 11:11:58 +0800

    practice 03

---
+++ christmas tree.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int b;
+
+    printf("輸入顯示層數\n");
+    scanf("%d",&b);
+
+    for(int i=1;i<=b;i++)
+    {
+        for(int j=1;j<=b-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=1;k<=i*2-1;k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for (int j=0;j<b/3;j++) {
+        for(int i=0;i<b-2;i++)
+        {
+            printf(" ");//印出底座左邊的空白
+        }
+       for(int i=0;i<3;i++)
+        {
+            printf("*");//印出底座
+        }
+        printf("\n");
+    }
+}
+
+
+
