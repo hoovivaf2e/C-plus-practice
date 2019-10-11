708815a47497439827534a6e4d98d9dc9060328e
Eileen <hooviva77@gmail.com>
2019-10-11 11:11:41 +0800

    practice 01

---
+++ hello world.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+
+    printf("輸入一個數字\n");
+    cin>>number;
+    cout<<number<<"Hello World!";
+
+    return 0;
+}
+
+
+
